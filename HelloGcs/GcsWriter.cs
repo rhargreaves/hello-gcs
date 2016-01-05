6989dbcc04af98367b4441544e0b08b2684e1fe9
Robert Hargreaves <kiyouta@gmail.com>
2016-01-05 22:48:59 +0000

    Can write object to GCS

---
+++ HelloGcs/GcsWriter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Http;
+using Google.Apis.Services;
+using Google.Apis.Storage.v1;
+
+namespace HelloGcs
+{
+    public class GcsWriter
+    {
+        private IConfigurableHttpClientInitializer GetApplicationDefaultCredentials()
+        {
+            // Loads credentials from file pointed to by GOOGLE_APPLICATION_CREDENTIALS environmental variable.
+            var credential = GoogleCredential.GetApplicationDefaultAsync().Result;
+            if (credential.IsCreateScopedRequired)
+            {
+                credential = credential.CreateScoped(StorageService.Scope.DevstorageReadWrite);
+            }
+            return credential;
+        }
+
+        public bool WriteStream(Stream stream, string name, string bucketName)
+        {
+            StorageService service = new StorageService(
+               new BaseClientService.Initializer()
+               {
+                   HttpClientInitializer = GetApplicationDefaultCredentials(),
+                   ApplicationName = "GCS Test",
+               });
+
+            var fileobj = new Google.Apis.Storage.v1.Data.Object()
+                {
+                    Name = name
+                };
+
+            service.Objects.Insert(fileobj, bucketName, stream, "text/plain").Upload();
+
+
+            return true;
+        }
+    }
+}
---
+++ Tests/Tests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using HelloGcs;
+
+namespace Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Can_write_file_to_GCS()
+        {
+            var stream = new MemoryStream(Encoding.UTF8.GetBytes("foo"));
+            var writer = new GcsWriter();
+            var result = writer.WriteStream(stream, "bar", "robert-hargreaves-test2");
+            Assert.That(result, Is.True);
+        }
+    }
+}
